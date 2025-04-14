27872ab659852392e9ef8408c70a2d4d48785802
nhstung <nhstung99@gmail.com>
2025-04-15 01:08:06 +0700

    Update server.js

--- my-chat-app/server.js
+++ my-chat-app/server.js
@@ -4,52 +4,37 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 
-// Kết nối tới MongoDB Atlas
-mongoose.connect('mongodb+srv://nhstung99:nhstung2403%40%40%40@cluster0.y6quevr.mongodb.net/?retryWrites=true&w=majority', {
+// Kết nối MongoDB (nếu có lưu tin nhắn)
+mongoose.connect('mongodb+srv://nhstung99:nhstung2403%40%40%40@cluster0.y6quevr.mongodb.net/chatApp?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Failed to connect to MongoDB:', err));
+.catch(err => console.error('MongoDB connection error:', err));
 
-// Định nghĩa model tin nhắn
-const Message = mongoose.model('Message', {
-  content: String
-});
+// (Nếu cần lưu tin nhắn, tạo model Message ở đây)
+// const Message = mongoose.model('Message', new mongoose.Schema({ content: String }));
 
-// Serve file tĩnh từ thư mục 'public'
 app.use(express.static('public'));
 
-// Xử lý socket.io
 io.on('connection', (socket) => {
-  // Khi user mới vào, gửi lại tin nhắn cũ
-  Message.find().then(messages => {
-    messages.forEach(msg => {
-      socket.emit('chat message', { text: msg.content, self: false });
-    });
+  // Khi nhận sự kiện đăng nhập
+  socket.on('user login', (data) => {
+    socket.username = data.username;
+    console.log(${data.username} đã đăng nhập);
+    // Phát tới tất cả người dùng thông báo rằng người đó đã vào
+    io.emit('user login', { username: data.username });
   });
 
-  // Khi user gửi tin nhắn mới
-  socket.on('chat message', async (data) => {
-    try {
-      // Lưu tin nhắn vào database
-      await Message.create({ content: data.text });
-
-      // Gửi lại cho chính mình
-      socket.emit('chat message', { text: data.text, self: true });
-
-      // Gửi cho những người khác
-      socket.broadcast.emit('chat message', { text: data.text, self: false });
-
-    } catch (err) {
-      console.error('Error saving message', err);
-    }
+  // Xử lý tin nhắn chat đơn giản
+  socket.on('chat message', (data) => {
+    // (Nếu lưu database, lưu tin ở đây)
+    io.emit('chat message', data);
   });
 
   console.log('A user connected');
 });
 
-// Khởi động server
 http.listen(process.env.PORT || 3000, () => {
-  console.log('Server listening...');
+  console.log('Server listening on port 3000');
 });
